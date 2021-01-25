a653e3af5efcc6290a88b64a30aa4dd6ea42a7c9
fleurdhelice <fleurdhelice@live.ru>
2021-01-25 20:24:53 +0300

    Update topbutton.js

--- topbutton.js
+++ topbutton.js
@@ -2,15 +2,15 @@
 mybutton = document.getElementById("myTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
