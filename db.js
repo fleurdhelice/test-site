421d035c155cf027b944bdd7d05655ac1ab18544
fleurdhelice <fleurdhelice@live.ru>
2021-02-13 19:37:52 +0300

    added papers to db

--- db.js
+++ db.js
@@ -205,13 +205,95 @@ var db = [
 	"tagYear" : ["2020"],
 	"tagPaperType" : ["talk"],
 	"tagLang" : ["eng"],
-	"tagObjectLangs" : ["lat"], //ACHTUNG
+	"tagObjectLangs" : ["lat"],
 	"tagArea" : ["digital humanities"], 
 	"tagKeywords" : ["distributional semantics"], //TODO
 	"bibtex" : "GeneralovaKondakova2018TalkLatinPeriodization",	
 	"download" : "https://www.academia.edu/39353107/An_approach_to_language_periodization_based_on_word_usage_Looking_for_a_pivot_point_in_Latin", 	
-}
+}, {
+	"title" : "V. Generalova and <a href="https:\/\/user.phil.hhu.de/petitjean/" target="blank">S. Petitjean</a> , <strong>A prototype of a metagrammar describing three-argument constructions with a morphological causative</strong>, Typology of Morphosyntactic Parameters, Vol. 3.2, pp. 29-51, 2020.",
+	"tagYear" : ["2020"],
+	"tagPaperType" : ["article"],
+	"tagLang" : ["eng"],
+	"tagObjectLangs" : ["bak", "xal", "bxk", "kbd", "niv"], 
+	"tagArea" : [], //TODO
+	"tagKeywords" : [], //TODO
+	"bibtex" : "GeneralovaPetitjean2020ProtoMG",	
+	"download" : "http://tmp.sc/application/files/8816/1186/2233/Generalova-Petitjean-2020-3-2.pdf", 	
 
+}, {
+	"title" : "V. Generalova, <strong>Vybor strategii oformlenia sentencial'nyx aktantov v baškirskom jazyke [Strategies of complement clauses marking in Bashkir]</strong>, Acta Linguistica Petropolitana, Vol. 12.1, pp. 323-335, 2016.",
+	"tagYear" : ["2016"],
+	"tagPaperType" : ["article"],
+	"tagLang" : ["rus"],
+	"tagObjectLangs" : ["bak"],
+	"tagArea" : ["syntax"], 
+	"tagKeywords" : ["complement clauses"], 
+	"bibtex" : "Generalova2016BashkirComplement",	
+	"download" : "https://www.academia.edu/40044846/%D0%92%D1%8B%D0%B1%D0%BE%D1%80_%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%B5%D0%B3%D0%B8%D0%B8_%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F_%D1%81%D0%B5%D0%BD%D1%82%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85_%D0%B0%D0%BA%D1%82%D0%B0%D0%BD%D1%82%D0%BE%D0%B2_%D0%B2_%D0%B1%D0%B0%D1%88%D0%BA%D0%B8%D1%80%D1%81%D0%BA%D0%BE%D0%BC_%D1%8F%D0%B7%D1%8B%D0%BA%D0%B5",
+}, {
+	"title" : "V. Generalova, <strong>Towards a cross-linguistic description of morphological causatives: issues in syntax-semantics linking</strong>, In: A. Pavlova (ed.) Proceedings of the ESSLLI & WeSSLLI Student Session 2020, Brandies University, pp. 55-66, 2020.",
+	"tagYear" : ["2020"],
+	"tagPaperType" : ["proceedings paper"],
+	"tagLang" : ["eng"],
+	"tagObjectLangs" : ["niv", "bak", "hur"], 
+	"tagArea" : ["syntax", "typology"], 
+	"tagKeywords" : ["causative", "verbal derivation"], 
+	"bibtex" : "Generalova2020CausativesLinking",	
+	"download" : "https://www.academia.edu/43943952/Towards_a_cross_linguistic_description_of_morphological_causatives_issues_in_syntax_semantics_linking", 
+}, {
+	"title" : "V. Generalova, <strong>Making war Slavic-like, or just choosing a preposition</strong>, In: M. Simeunovich-Skvortsova et al. (eds.) VARIA XXIV. Zborník príspevkov z XXIV. kolokvia mladých jazykovedcov. (Praha 9. – 11. 9. 2015) [Proceedings of the 24th Colloquium of Young Linguists in Prague, 9-11 September 2015], pp. 96-106, 2019.",
+	"tagYear" : ["2019"],
+	"tagPaperType" : ["proceedings paper"],
+	"tagLang" : ["eng"],
+	"tagObjectLangs" : ["rus", "ces", "pol"], 
+	"tagArea" : ["syntax"], 
+	"tagKeywords" : ["verbs of counteraction"], 
+	"bibtex" : "Generalova2019WarSlavicLike",	
+	"download" : "https://www.juls.savba.sk/ediela/varia/24/Varia24.pdf",	
+}, {
+	"title" : "V. Generalova, <strong>Differencial'noe markirovanie subjekta v konstrukcijax s sentencial'nymi aktantami [Differencial subject marking in Bashkir constructions with sentential agents]</strong>, In: Materialy XIV Vserossijskoj nauchnoj konferencii «Aktual'nye problemy dialektologii jazykov narodov Rossii» [Proceedings of the 14th Conference «Actual Problems of Dialectology of Minor Languages in Russia»], pp. 130-136, 2014.",
+	"tagYear" : ["2014"],
+	"tagPaperType" : ["proceedings paper"],
+	"tagLang" : ["rus"],
+	"tagObjectLangs" : ["bak"], 
+	"tagArea" : ["syntax"], 
+	"tagKeywords" : ["complement clauses"], //TODO check uniformity, check again translation
+	"bibtex" : "Generalova2014SubjectMarkingBashkir",	
+	"download" : "", //ACHTUNG
+}, {
+	"title" : "V. Generalova, <strong>Aktanty motivirujuš'ego glagola v semantike russkix otglagol'nyx prilagatel'nyx [Arguments of motivating verbs in the semantics of Russian deverbal adjectives]</strong>, B. A. Thesis, St Petersburg State University, 2016.",
+	"tagYear" : ["2016"],
+	"tagPaperType" : ["thesis"],
+	"tagLang" : ["rus"],
+	"tagObjectLangs" : ["rus"], 
+	"tagArea" : ["syntax"], 
+	"tagKeywords" : ["deverbal adjectives"], 
+	"bibtex" : "Generalova2012BAThesis",	
+	"download" : "https://dspace.spbu.ru/bitstream/11701/3518/1/Valera_Diplom_final.pdf", 
+}, {
+	"title" : "V. Generalova, <strong>Avtomatizirovannoe izvlečenie replik, svidetel'stvujuš'ih ob otricatel'noj ocenke sobesednika, iz čeloveko-mašinnyx dialogov različnoj tematiki [Automated extraction of negative feedback from human-bot dialogues]</strong>, M. A. Thesis, Russian State University for the Humanities, Moscow, 2018.",
+	"tagYear" : ["2018"],
+	"tagPaperType" : ["thesis"],
+	"tagLang" : ["rus"],
+	"tagObjectLangs" : ["rus"], 
+	"tagArea" : [], //TODO
+	"tagKeywords" : [], //TODO 
+	"bibtex" : "Generalova2018MAThesis",	
+	"download" : "", //ACHTUNG
+
+}, {
+	"title" : "V. Generalova, <strong>Towards an RRG analysis of constructions with a morphological causative in some Altaic languages</strong>, In: A. Tigău (ed.) The Syntax and Semantics of Internal Arguments. Special issue of Bucharest Working Papers in Linguistics, Vol. 22.2"
+	"tagYear" : ["to appear"],
+	"tagPaperType" : ["article"],
+	"tagLang" : ["eng"],
+	"tagObjectLangs" : ["bak", "xal", "kjh", "tuv"], 
+	"tagArea" : [], //TODO
+	"tagKeywords" : [], //TODO 
+	"bibtex" : "GeneralovaBWPL",	
+	"download" : "", //ACHTUNG
+
+}
 
 ]
 
