21f4fb32e609f5d6f6255c41e3b1e98dc8b7c8f4
fleurdhelice <fleurdhelice@live.ru>
2021-02-17 05:43:57 +0300

    real industrial cv added

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ var db = [
 	"tagArea" : ["semantics"],
 	"tagKeywords" : ["stereotype"],
 	"bibtex" : "Generalova2013TalkStereotypical",
-	"download" : "", //ACHTUNG
+	"download" : "" 
 },{
 	"title" : "V. Generalova, <strong>Choice of prepositions \'with\' and \'against\' in syntactic structures with verbs defining counteraction</strong>, Talk at the XXII “Lomonosov” Conference of Young Researches, Moscow State University, Russia, Apr. 2015.",
 	"tagYear" : ["2015"],
@@ -18,7 +18,7 @@ var db = [
 	"tagArea" : ["syntax"],
 	"tagKeywords" : ["verbs of counteraction"],
 	"bibtex" : "Generalova2015TalkCounteractionPrepChoice",
-	"download" : "/test-site/papers/Generalova2015TalkCounteractionPrepChoice_abstract.pdf",
+	"download" : "/test-site/papers/Generalova2015TalkCounteractionPrepChoice_abstract.pdf"
 },{
 	"title" : "V. Generalova, <strong>Human-machine interaction: 50 years of evolution</strong>, Talk at the Motorola Solutions Tech Expo 2015, St Petersburg, Russia; Krakow, Poland; Tel-Aviv, Israel (stream): Motorola Solutions Inc., Oct. 2015.",
 	"tagYear" : ["2015"],
@@ -28,7 +28,7 @@ var db = [
 	"tagArea" : ["human-machine interaction"], 
 	"tagKeywords" : ["chatbots"], 
 	"bibtex" : "Generalova2015Talk50Years",
-	"download" : "", //ACHTUNG
+	"download" : "" 
 }, {
 	"title" : "V. Generalova, <strong>Making war slavic-like</strong>, Talk at the XXIV Colloquium of Young Linguists, Prague, Czech Republic, Sep. 2015.",
 	"tagYear" : ["2015"],
@@ -38,7 +38,7 @@ var db = [
 	"tagArea" : ["syntax"],
 	"tagKeywords" : ["verbs of counteraction"], 	
 	"bibtex" : "Generalova2015TalkWarSlavic",
-	"download" : "", //ACHTUNG
+	"download" : "" 
 }, {
 	"title" : "V. Generalova, <strong>With and against whom making war is better</strong>, Keynote session talk at the XVIII Conference of Young Linguists, St Petersburg State University, Russia, Apr. 2015.",
 	"tagYear" : ["2015"],
@@ -48,7 +48,7 @@ var db = [
 	"tagArea" : ["syntax"],
 	"tagKeywords" : ["verbs of counteraction"], 
 	"bibtex" : "Generalova2015TalkWithAgainst",	
-	"download" : "/test-site/papers/Generalova2015TalkWithAgainst_abstract.pdf",
+	"download" : "/test-site/papers/Generalova2015TalkWithAgainst_abstract.pdf"
 }, {
 	"title" : "V. Generalova, <strong>Lability of Russian deverbal adjectives</strong>, Talk at the XIX Conference of Young Linguists, St Petersburg State University, Russia, Apr. 2016.",
 	"tagYear" : ["2016"],
@@ -58,7 +58,7 @@ var db = [
 	"tagArea" : ["syntax", "morphology"],
 	"tagKeywords" : ["deverbal adjectives"], 
 	"bibtex" : "Generalova2016TalkDevAdjLabil",	
-	"download" : "/test-site/papers/Generalova2016TalkDevAdjLabil_program.pdf",
+	"download" : "/test-site/papers/Generalova2016TalkDevAdjLabil_program.pdf"
 }, {
 	"title" : "V. Generalova, <strong>Paradigmatic relationship between Russian deverbal adjectives</strong>, Talk at the 49th Annual Meeting of the Societas Linguistica Europaea (SLE), Naples, Italy, Sep. 2016.",
 	"tagYear" : ["2016"],
@@ -68,7 +68,7 @@ var db = [
 	"tagArea" : ["syntax", "morphology"],
 	"tagKeywords" : ["deverbal adjectives"], 
 	"bibtex" : "Generalova2016TalkDevAdjParadigm",	
-	"download" : "https://www.academia.edu/28251949/Paradigmatic_relationship_between_Russian_deverbal_adjectives",
+	"download" : "https://www.academia.edu/28251949/Paradigmatic_relationship_between_Russian_deverbal_adjectives"
 }, {
 	"title" : "V. Generalova, <strong>Markup of human-bot dialogues: Known problems and new challenges</strong>, Talk at the 61st Student’s Conference for Linguistics (StuTS), Zurich, Switzerland, May 2017.",
 	"tagYear" : ["2017"],
@@ -78,7 +78,7 @@ var db = [
 	"tagArea" : ["human-machine interaction"], 
 	"tagKeywords" : ["chatbots"], 
 	"bibtex" : "Generalova2017TalkDialogsMarkup",	
-	"download" : "/test-site/papers/Generalova2017TalkDialogsMarkup_slides.pdf",	
+	"download" : "/test-site/papers/Generalova2017TalkDialogsMarkup_slides.pdf"
 }, {
 	"title" : "V. Generalova, <strong>Russian deverbal adjectives as relativization devices</strong>, Poster presentation at the 7th Annual Conference of Undergraduate Linguistics Association of Britain (ULAB), Cambridge, UK, May 2017.",
 	"tagYear" : ["2017"],
@@ -88,7 +88,7 @@ var db = [
 	"tagArea" : ["syntax"], 
 	"tagKeywords" : ["deverbal adjectives"], 
 	"bibtex" : "Generalova2017TalkDevAdjRel",	
-	"download" : "Generalova2017TalkDevAdjRel_program.pdf",	
+	"download" : "Generalova2017TalkDevAdjRel_program.pdf"
 }, {
 	"title" : "V. Generalova, <strong>Shall we or shall we not? stop words in distributional semantics from a comparative perspective</strong>, Talk at the Rencontre des jeunes chercheurs 2018, Paris, France, May 2018.",
 	"tagYear" : ["2018"],
@@ -98,7 +98,7 @@ var db = [
 	"tagArea" : ["semantics", "computational"], 
 	"tagKeywords" : ["distributional semantics"], 
 	"bibtex" : "Generalova2018TalkStopWords",	
-	"download" : "", //ACHTUNG
+	"download" : "" 
 }, {
 	"title" : "V. Generalova, <strong>Spontaneously funny dialogues in human-machine communication</strong>, Talk at The 30th International Society for Humor Studies (ISHS) Conference, Tallinn, Estonia, Jun. 2018.",
 	"tagYear" : ["2018"],
@@ -108,7 +108,7 @@ var db = [
 	"tagArea" : ["human-machine interaction", "machine learning"], 
 	"tagKeywords" : ["chatbots"], 
 	"bibtex" : "Generalova2018TalkFunnyDialogues",	
-	"download" : "/test-site/papers/Generalova2018TalkFunnyDialogues_slides.pdf",	
+	"download" : "/test-site/papers/Generalova2018TalkFunnyDialogues_slides.pdf"	
 }, {
 	"title" : "V. Generalova, <strong>An RRG approach to case assignment in Altaic causative constructions</strong>, Talk at The International Workshop “The syntax and semantics of internal arguments”, Bucharest, Romania, Oct. 2019.",
 	"tagYear" : ["2018"],
@@ -118,7 +118,7 @@ var db = [
 	"tagArea" : ["syntax", "RRG"], 
 	"tagKeywords" : ["verbal derivation", "causative"], 
 	"bibtex" : "Generalova2019TalkAltaicCaus",	
-	"download" : "/test-site/papers/Generalova2019TalkAltaicCaus_slides.pdf",	
+	"download" : "/test-site/papers/Generalova2019TalkAltaicCaus_slides.pdf"
 }, {
 	"title" : "V. Generalova, <strong>An RRG analysis of Halkomelem ditransitive constructions: Integrating head-marking and dependent-marking properties</strong>, Talk at the International Conference on Role and Reference Grammar 2019, Buffalo, NY, USA, Aug. 2019",
 	"tagYear" : ["2019"],
@@ -128,7 +128,7 @@ var db = [
 	"tagArea" : ["syntax", "RRG"], 
 	"tagKeywords" : ["ditransitive"], 
 	"bibtex" : "Generalova2019TalkHalkHeadDep",	
-	"download" : "https://www.academia.edu/40146436/An_RRG_analysis_of_Halkomelem_ditransitive_constructions_Integrating_head_marking_and_dependent_marking_properties",	
+	"download" : "https://www.academia.edu/40146436/An_RRG_analysis_of_Halkomelem_ditransitive_constructions_Integrating_head_marking_and_dependent_marking_properties"
 
 }, {
 	"title" : "V. Generalova, <strong>Argument linking in some Turkic causative constructions</strong>, Talk at the TreeGraSP Meeting #3, Heinrich Heine University of Dusseldorf, May 2019.",
@@ -139,7 +139,7 @@ var db = [
 	"tagArea" : ["syntax", "RRG"], 
 	"tagKeywords" : ["verbal derivation", "causative", "argument linking"], 
 	"bibtex" : "Generalova2019TalkArgLinkTurkicCaus",	
-	"download" : "https://treegrasp.phil.hhu.de/wp-content/uploads/2019/05/meeting_3_argument_linking.pdf",		
+	"download" : "https://treegrasp.phil.hhu.de/wp-content/uploads/2019/05/meeting_3_argument_linking.pdf"
 }, {
 	"title" : "V. Generalova, <strong>A metagrammar of three-argument constructions with morphological causatives: General design and first implementations</strong>, Talk at the TreeGraSP Meeting #5, Heinrich Heine University of Dusseldorf, Nov. 2020.",
 	"tagYear" : ["2020"],
@@ -149,7 +149,7 @@ var db = [
 	"tagArea" : ["syntax", "semantics", "RRG"], 
 	"tagKeywords" : ["verbal derivation", "causative", "metagrammar"], 
 	"bibtex" : "Generalova2020TalkMetagrammarFirstImpl",	
-	"download" : "https://treegrasp.phil.hhu.de/wp-content/uploads/2020/11/meeting_5_metagrammar.pdf",	
+	"download" : "https://treegrasp.phil.hhu.de/wp-content/uploads/2020/11/meeting_5_metagrammar.pdf"	
 }, {
 	"title" : "V. Generalova, <strong>Modeling the variety of constructions with morphological causatives metagrammatically: A first sketch</strong>, Talk at the 10th conference \"Typology of Morphosyntactic Parameters\", Moscow, Russia (online), Oct. 2020.",
 	"tagYear" : ["2020"],
@@ -159,7 +159,7 @@ var db = [
 	"tagArea" : ["syntax", "semantics", "RRG"], 
 	"tagKeywords" : ["verbal derivation", "causative", "metagrammar"], 
 	"bibtex" : "Generalova2020TalkMetagrammarSketch",	
-	"download" : "", //ACHTUNG	
+	"download" : "" 	
 }, {
 	"title" : "V. Generalova, <strong>A verbal alternation construction without macrorole change: Comparing approaches to linking in RRG</strong>, Poster presentation at the 21st Workshop on the Roots of Pragmasemantics, Szklarska Poręba, Poland, Feb. 2020.",
 	"tagYear" : ["2020"],
@@ -169,7 +169,7 @@ var db = [
 	"tagArea" : ["syntax", "semantics", "RRG"], 
 	"tagKeywords" : ["verbal derivation", "causative", "argument linking"], 
 	"bibtex" : "Generalova2020TalkMacroroleAltern",	
-	"download" : "/test-site/papers/Generalova2020TalkMacroroleAltern_poster.pdf", 	
+	"download" : "/test-site/papers/Generalova2020TalkMacroroleAltern_poster.pdf"	
 }, {
 	"title" : "V. Generalova, <strong>Applicatives and transitivizers as valence-increasing or valence-maintaining devices in Salish</strong>, Talk at the Workshop “Neglected syntactic functions and non-syntactic functions of Applicative Morphology”, SLE Annual Meeting 2020, Online, Aug. 2020.",
 	"tagYear" : ["2020"],
@@ -179,7 +179,7 @@ var db = [
 	"tagArea" : ["syntax", "semantics"], 
 	"tagKeywords" : ["verbal derivation", "applicative"], 
 	"bibtex" : "Generalova2020TalkSalishApplicatives",	
-	"download" : "", //ACHTUNG 	
+	"download" : ""  	
 }, {
 	"title" : "V. Generalova, <strong>Towards a cross-linguistic description of morphological causatives: Issues in syntax-semantics linking</strong>, Talk at the WeSSLLI + ESSLLI Virtual Student Session, Online, Jul. 2020.",
 	"tagYear" : ["2020"],
@@ -189,37 +189,37 @@ var db = [
 	"tagArea" : ["syntax", "semantics", "RRG"], 
 	"tagKeywords" : ["verbal derivation", "causative", "metagrammar", "argument linking"], 
 	"bibtex" : "Generalova2020TalkCausSySeLinking",	
-	"download" : "", //ACHTUNG	
+	"download" : "" 	
 }, {
 	"title" : "V. Generalova, <strong>Yet another attempt at a syntax-driven typology of morphological causative constructions</strong>, Talk at the 17th Conference on Typology and Grammar for Young Scholars, St Petersburg, Russia (online), Nov. 2020.",
 	"tagYear" : ["2020"],
 	"tagPaperType" : ["talk"],
 	"tagLang" : ["eng"],
-	"tagObjectLangs" : [""], //ACHTUNG
+	"tagObjectLangs" : [""], 
 	"tagArea" : ["syntax", "typology"], 
 	"tagKeywords" : ["causative"], 
 	"bibtex" : "Generalova2020TalkTypologyCaus",	
-	"download" : "", //ACHTUNG	
+	"download" : "" 	
 }, {
-	"title" : "V. Generalova and <a href="https:\/\/oxford.academia.edu/DariaKondakova" target="blank">D. Kondakova</a> , <strong>An approach to language periodization based on word usage: Looking for a pivot point in Latin</strong>, Talk at the DH Benelux 2018, Amsterdam, Netherlands, Jun. 2018.",
+	"title" : "V. Generalova and D. Kondakova, <strong>An approach to language periodization based on word usage: Looking for a pivot point in Latin</strong>, Talk at the DH Benelux 2018, Amsterdam, Netherlands, Jun. 2018.", //TODO add link to Kondakova
 	"tagYear" : ["2020"],
 	"tagPaperType" : ["talk"],
 	"tagLang" : ["eng"],
 	"tagObjectLangs" : ["lat"],
 	"tagArea" : ["digital humanities"], 
-	"tagKeywords" : ["distributional semantics"], //TODO
+	"tagKeywords" : ["distributional semantics"], 
 	"bibtex" : "GeneralovaKondakova2018TalkLatinPeriodization",	
-	"download" : "https://www.academia.edu/39353107/An_approach_to_language_periodization_based_on_word_usage_Looking_for_a_pivot_point_in_Latin", 	
+	"download" : "https://www.academia.edu/39353107/An_approach_to_language_periodization_based_on_word_usage_Looking_for_a_pivot_point_in_Latin" 	
 }, {
-	"title" : "V. Generalova and <a href="https:\/\/user.phil.hhu.de/petitjean/" target="blank">S. Petitjean</a> , <strong>A prototype of a metagrammar describing three-argument constructions with a morphological causative</strong>, Typology of Morphosyntactic Parameters, Vol. 3.2, pp. 29-51, 2020.",
+	"title" : "V. Generalova and S. Petitjean, <strong>A prototype of a metagrammar describing three-argument constructions with a morphological causative</strong>, Typology of Morphosyntactic Parameters, Vol. 3.2, pp. 29-51, 2020.", //TODO add link to Petitjean
 	"tagYear" : ["2020"],
 	"tagPaperType" : ["article"],
 	"tagLang" : ["eng"],
 	"tagObjectLangs" : ["bak", "xal", "bxk", "kbd", "niv"], 
-	"tagArea" : [], //TODO
-	"tagKeywords" : [], //TODO
+	"tagArea" : [], 
+	"tagKeywords" : [], 
 	"bibtex" : "GeneralovaPetitjean2020ProtoMG",	
-	"download" : "http://tmp.sc/application/files/8816/1186/2233/Generalova-Petitjean-2020-3-2.pdf", 	
+	"download" : "http://tmp.sc/application/files/8816/1186/2233/Generalova-Petitjean-2020-3-2.pdf"
 
 }, {
 	"title" : "V. Generalova, <strong>Vybor strategii oformlenia sentencial'nyx aktantov v baškirskom jazyke [Strategies of complement clauses marking in Bashkir]</strong>, Acta Linguistica Petropolitana, Vol. 12.1, pp. 323-335, 2016.",
@@ -230,7 +230,7 @@ var db = [
 	"tagArea" : ["syntax"], 
 	"tagKeywords" : ["complement clauses"], 
 	"bibtex" : "Generalova2016BashkirComplement",	
-	"download" : "https://www.academia.edu/40044846/%D0%92%D1%8B%D0%B1%D0%BE%D1%80_%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%B5%D0%B3%D0%B8%D0%B8_%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F_%D1%81%D0%B5%D0%BD%D1%82%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85_%D0%B0%D0%BA%D1%82%D0%B0%D0%BD%D1%82%D0%BE%D0%B2_%D0%B2_%D0%B1%D0%B0%D1%88%D0%BA%D0%B8%D1%80%D1%81%D0%BA%D0%BE%D0%BC_%D1%8F%D0%B7%D1%8B%D0%BA%D0%B5",
+	"download" : "https://www.academia.edu/40044846/%D0%92%D1%8B%D0%B1%D0%BE%D1%80_%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%B5%D0%B3%D0%B8%D0%B8_%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F_%D1%81%D0%B5%D0%BD%D1%82%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85_%D0%B0%D0%BA%D1%82%D0%B0%D0%BD%D1%82%D0%BE%D0%B2_%D0%B2_%D0%B1%D0%B0%D1%88%D0%BA%D0%B8%D1%80%D1%81%D0%BA%D0%BE%D0%BC_%D1%8F%D0%B7%D1%8B%D0%BA%D0%B5"
 }, {
 	"title" : "V. Generalova, <strong>Towards a cross-linguistic description of morphological causatives: issues in syntax-semantics linking</strong>, In: A. Pavlova (ed.) Proceedings of the ESSLLI & WeSSLLI Student Session 2020, Brandies University, pp. 55-66, 2020.",
 	"tagYear" : ["2020"],
@@ -250,7 +250,7 @@ var db = [
 	"tagArea" : ["syntax"], 
 	"tagKeywords" : ["verbs of counteraction"], 
 	"bibtex" : "Generalova2019WarSlavicLike",	
-	"download" : "https://www.juls.savba.sk/ediela/varia/24/Varia24.pdf",	
+	"download" : "https://www.juls.savba.sk/ediela/varia/24/Varia24.pdf"	
 }, {
 	"title" : "V. Generalova, <strong>Differencial'noe markirovanie subjekta v konstrukcijax s sentencial'nymi aktantami [Differencial subject marking in Bashkir constructions with sentential agents]</strong>, In: Materialy XIV Vserossijskoj nauchnoj konferencii «Aktual'nye problemy dialektologii jazykov narodov Rossii» [Proceedings of the 14th Conference «Actual Problems of Dialectology of Minor Languages in Russia»], pp. 130-136, 2014.",
 	"tagYear" : ["2014"],
@@ -258,9 +258,9 @@ var db = [
 	"tagLang" : ["rus"],
 	"tagObjectLangs" : ["bak"], 
 	"tagArea" : ["syntax"], 
-	"tagKeywords" : ["complement clauses"], //TODO check uniformity, check again translation
+	"tagKeywords" : ["complement clauses"], 
 	"bibtex" : "Generalova2014SubjectMarkingBashkir",	
-	"download" : "", //ACHTUNG
+	"download" : "" 
 }, {
 	"title" : "V. Generalova, <strong>Aktanty motivirujuš'ego glagola v semantike russkix otglagol'nyx prilagatel'nyx [Arguments of motivating verbs in the semantics of Russian deverbal adjectives]</strong>, B. A. Thesis, St Petersburg State University, 2016.",
 	"tagYear" : ["2016"],
@@ -270,30 +270,28 @@ var db = [
 	"tagArea" : ["syntax"], 
 	"tagKeywords" : ["deverbal adjectives"], 
 	"bibtex" : "Generalova2012BAThesis",	
-	"download" : "https://dspace.spbu.ru/bitstream/11701/3518/1/Valera_Diplom_final.pdf", 
+	"download" : "https://dspace.spbu.ru/bitstream/11701/3518/1/Valera_Diplom_final.pdf"
 }, {
 	"title" : "V. Generalova, <strong>Avtomatizirovannoe izvlečenie replik, svidetel'stvujuš'ih ob otricatel'noj ocenke sobesednika, iz čeloveko-mašinnyx dialogov različnoj tematiki [Automated extraction of negative feedback from human-bot dialogues]</strong>, M. A. Thesis, Russian State University for the Humanities, Moscow, 2018.",
 	"tagYear" : ["2018"],
 	"tagPaperType" : ["thesis"],
 	"tagLang" : ["rus"],
 	"tagObjectLangs" : ["rus"], 
-	"tagArea" : [], //TODO
-	"tagKeywords" : [], //TODO 
+	"tagArea" : [], 
+	"tagKeywords" : [], 
 	"bibtex" : "Generalova2018MAThesis",	
-	"download" : "", //ACHTUNG
+	"download" : "" 
 
 }, {
-	"title" : "V. Generalova, <strong>Towards an RRG analysis of constructions with a morphological causative in some Altaic languages</strong>, In: A. Tigău (ed.) The Syntax and Semantics of Internal Arguments. Special issue of Bucharest Working Papers in Linguistics, Vol. 22.2"
+	"title" : "V. Generalova, <strong>Towards an RRG analysis of constructions with a morphological causative in some Altaic languages</strong>, In: A. Tigău (ed.) The Syntax and Semantics of Internal Arguments. Special issue of Bucharest Working Papers in Linguistics, Vol. 22.2",
 	"tagYear" : ["to appear"],
 	"tagPaperType" : ["article"],
 	"tagLang" : ["eng"],
 	"tagObjectLangs" : ["bak", "xal", "kjh", "tuv"], 
-	"tagArea" : [], //TODO
-	"tagKeywords" : [], //TODO 
+	"tagArea" : [], 
+	"tagKeywords" : [],
 	"bibtex" : "GeneralovaBWPL",	
-	"download" : "", //ACHTUNG
+	"download" : "" 
 
-}
-
-]
+} ]
 
