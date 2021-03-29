ef5f78aeeeeaea63b6f232e8f67d75d9542b366e
fleurdhelice <fleurdhelice@live.ru>
2021-03-29 10:08:30 +0300

    Update db.js

--- db.js
+++ db.js
@@ -53,13 +53,13 @@ var db = [
 	"bibtex" : "Generalova2016BashkirComplement",	
 	"download" : "https://www.academia.edu/40044846/%D0%92%D1%8B%D0%B1%D0%BE%D1%80_%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%B5%D0%B3%D0%B8%D0%B8_%D0%BE%D1%84%D0%BE%D1%80%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F_%D1%81%D0%B5%D0%BD%D1%82%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85_%D0%B0%D0%BA%D1%82%D0%B0%D0%BD%D1%82%D0%BE%D0%B2_%D0%B2_%D0%B1%D0%B0%D1%88%D0%BA%D0%B8%D1%80%D1%81%D0%BA%D0%BE%D0%BC_%D1%8F%D0%B7%D1%8B%D0%BA%D0%B5"
 }, {
-	"title" : "V. Generalova, <strong>Towards a cross-linguistic description of morphological causatives: issues in syntax-semantics linking</strong>, In: A. Pavlova (ed.) Proceedings of the ESSLLI & WeSSLLI Student Session 2020, Brandies University, pp. 55-66, 2020.",
+	"title" : "V. Generalova, <strong>Towards a cross-linguistic description of morphological causatives: Issues in syntax-semantics linking</strong>, In: A. Pavlova (ed.) Proceedings of the ESSLLI & WeSSLLI Student Session 2020, Brandies University, pp. 55-66, 2020.",
 	"tagYear" : ["2020"],
 	"tagPaperType" : ["in proceedings"],
 	"tagLang" : ["eng"],
 	"tagObjectLangs" : ["niv", "bak", "hur"], 
 	"tagArea" : ["syntax", "typology"], 
-	"tagKeywords" : ["causatives", "valence", "metagrammar"], 
+	"tagKeywords" : ["valence", "causatives", "metagrammar", "argument linking"], 
 	"bibtex" : "Generalova2020CausativesLinking",	
 	"download" : "https://www.academia.edu/43943952/Towards_a_cross_linguistic_description_of_morphological_causatives_issues_in_syntax_semantics_linking", 
 }, {
